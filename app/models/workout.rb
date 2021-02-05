7f1a6ac0f74ed167b942b5dc28580076835f03b8
benikovacs <bncekovacs@gmail.com>
2021-02-05 13:11:04 +0000

    commit before users added

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,2 +1,6 @@
 class Workout < ApplicationRecord
+    validates :title, presence: true, length: {minimum:6, maximum:20} 
+    validates :duration, presence: true, length: {minimum:1, maximum:5}
+    validates :calories, presence: true, length: {minimum:1, maximum:5}
+    validates :note, length: {minimum:0, maximum:250}
 end
