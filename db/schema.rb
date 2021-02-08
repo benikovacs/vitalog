f8a40d71111d084eac1fb859f1c7a50f5190593c
benikovacs <bncekovacs@gmail.com>
2021-02-08 21:52:09 +0000

    signup naming renamed to rails conventions

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_143442) do
+ActiveRecord::Schema.define(version: 2021_02_05_172621) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
   create_table "workouts", force: :cascade do |t|
